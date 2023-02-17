f9afc2f7f1dc35e0b671a366202832f1aa1c79e3
kourosh <kourosh.mahmoudi.2004@gmail.com>
2023-02-17 20:49:53 +0330

    adding filter list

--- index.js
+++ index.js
@@ -2,10 +2,12 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
+const filterOption = document.querySelector(".filter-todo");
 
 //Event Listeners
 todoButton.addEventListener('click', addTodo);
 todoList.addEventListener('click', deleteCheck);
+filterOption.addEventListener('click', filterTodo);
 
 //Functions
 
@@ -65,3 +67,34 @@ function deleteCheck(Event){
     }
 } 
 
+//Filtering todos
+
+function filterTodo(Event){
+    const todos = todoList.childNodes;
+
+    todos.forEach(function(todo){
+
+        switch(Event.target.value){
+
+            case "all":
+                todo.style.display = "flex";
+                break;
+
+            case "completed":
+                if(todo.classList.contains("completed")){
+                    todo.style.display = "flex";
+                }else {
+                    todo.style.display = "none";
+                }
+                break;
+            
+            case "uncompleted":
+                if(!todo.classList.contains("completed")){
+                    todo.style.display = "flex";
+                }else {
+                    todo.style.display = "none";
+                }    
+                break;
+        }
+    });
+}
